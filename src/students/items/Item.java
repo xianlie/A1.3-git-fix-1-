0e190ca96bb4b82478252cc559929843cae51782
Xian Lie <lohxy002@mymail.unisa.edu.au>
2024-04-05 12:37:55 +1030

    item base class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,17 @@
 package students.items;
 
 public class Item {
+	protected int age;
+    protected int maturationAge;
+    protected int deathAge;
+    protected int monetaryValue;
+    
+    public Item(int maturationAge, int deathAge, int monetaryValue) {
+        this.age = 0;
+        this.maturationAge = maturationAge;
+        this.deathAge = deathAge;
+        this.monetaryValue = monetaryValue;
+    }
 
 	
 }
