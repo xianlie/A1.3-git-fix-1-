deedef9684c591e6385615b8c8aabd734d46d530
Xian Lie <lohxy002@mymail.unisa.edu.au>
2024-04-05 14:23:29 +1030

    adding comment + Grain class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -8,6 +8,7 @@ public abstract class Item {
     protected int deathAge;
     protected int monetaryValue;
     
+    // This is a constructor for creating an item with a specified maturation age, death age and monetary value.
     public Item(int maturationAge, int deathAge, int monetaryValue) {
         this.age = 0;
         this.maturationAge = maturationAge;
@@ -15,18 +16,22 @@ public abstract class Item {
         this.monetaryValue = monetaryValue;
     }
     
+    // This method advances the age of the item by one time unit.
     public void tick() {
         age++;
     }
-
+    
+    // This is a setter method used to set the age of the item.
     public void setAge(int age) {
         this.age = age;
     }
     
+    // This method checks whether the item has died.
     public boolean died() {
         return age > deathAge;
     }
-
+    
+    // This method retrieves the monetary value of the item.
     public int getValue() {
         if (age >= maturationAge) {
             return monetaryValue;
@@ -52,3 +57,26 @@ abstract class Food extends Item {
         super(maturationAge, deathAge, monetaryValue);
     }
 }
+
+class Grain extends Food{
+	private static int generationCount = 0;
+	
+	public Grain() {
+		super(2, 6 ,2);
+		generationCount++;
+	}
+	
+	public static int getGenerationCount() {
+		return generationCount;
+	}
+	
+	@Override
+	public String toString() {
+		if(age < maturationAge) {
+			return "g";
+		} else {
+			return "G";
+		}
+	}
+}
+
