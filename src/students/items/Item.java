3ff251fe20e13fdcd8d378fe380c32fbd6e42cf0
Xian Lie <lohxy002@mymail.unisa.edu.au>
2024-04-05 13:08:48 +1030

    item of functions p1

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -12,6 +12,26 @@ public class Item {
         this.deathAge = deathAge;
         this.monetaryValue = monetaryValue;
     }
+    
+    public void tick() {
+        age++;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public boolean died() {
+        return age > deathAge;
+    }
+
+    public int getValue() {
+        if (age >= maturationAge) {
+            return monetaryValue;
+        } else {
+            return 0;
+        }
+    }
 
 	
 }
