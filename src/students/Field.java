8af1bfed0c8bf505c0876c100095bab3f285f7a2
Xian Lie <lohxy002@mymail.unisa.edu.au>
2024-04-06 01:47:43 +1030

    create tick function

--- src/students/Field.java
+++ src/students/Field.java
@@ -8,12 +8,14 @@ public class Field {
 	private Item[][] grid;
 	private int height;
 	private int width;
+	private Random random;
 	
 	
 	public Field(int height, int width){
 		this.height = height;
 		this.width = width; 
 		this.grid = new Item[height][width];
+		this.random = new Random();
 		
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
@@ -21,5 +23,21 @@ public class Field {
 			}
 		}
 	}
-	
+		
+	public void tick() {
+		for (int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				Item item = grid[i][j];
+				item.tick();
+			
+			if (item instanceof Soil && random.nextDouble() < 0.2) {
+				grid[i][j] = new Weed();
+			}
+			
+			if (item.died()) {
+				grid[i][j] = new UntilledSoil();
+			}
+		  }
+		}
+	}		
 }
