da3dd2521687c2a491a45c09d60e951309c4afe4
Xian Lie <lohxy002@mymail.unisa.edu.au>
2024-04-06 22:08:58 +1030

    create a beginning Farm class

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -1,9 +1,17 @@
 package students;
 
+import java.util.Scanner;
+
+import students.items.*;
+
 public class Farm {
+	private Field field;
+	private int bankBalance;
 	
 	public Farm(int fieldWidth, int fieldHeight, int startingFunds)
 	{
+		this.field = new Field(fieldWidth, fieldHeight);
+		this.bankBalance = startingFunds;
 	}
 	
 	public void run()
